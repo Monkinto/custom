7bdaafd37c9ed9186809b9b75177d589b4f26c99
monkinto <master.monkinto@gmail.com>
2019-11-08 12:48:48 -0500

    more updates

--- install/install.sh
+++ install/install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+#Variables
+DERIVATIVE="$(cat /etc/release | grep "ID_LIKE=.*")"
+COMPTYPE="N"
+
+#Bring everything up to date
+update_arch () {
+	sudo pacman -Syu
+}
+
+update_ubuntu() {
+	sudo apt update
+	sudo apt upgrade -y
+}
+
+#beginning of the main script

92fadb377bd69d5b8ec3821369a214554001b734
Mike diBenedetto <master.monkinto@gmail.com>
2020-11-25 00:02:25 -0500

    Update setup.sh

--- Scripts/systemprep/setup.sh
+++ Scripts/systemprep/setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+###############################################################################
+#Variable Section
+PManager="Null"
+CompType=0
+
+###############################################################################
+#Function Section
+
+###############################################################################
+#Determine Variables
+#Package Manager Detection
 if ! command -v apt &> /dev/null
 then
 	echo "Package Manager apt detected"
@@ -9,3 +20,17 @@ if ! command -v pacman &> /dev/null
 then
 	echo "Package Manager pacman detected"
 fi
+
+#Prompt for System Type
+
+###############################################################################
+#Program Logic Section
+
+#Update System
+
+#Download Git Repo
+
+#Install All Programs
+
+#Create Symlinks
+

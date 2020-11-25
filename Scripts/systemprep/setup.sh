a912a7700c08f2248f446c432642d7dc0b609585
Mike diBenedetto <master.monkinto@gmail.com>
2020-11-25 13:53:42 -0500

    Update setup.sh

--- Scripts/systemprep/setup.sh
+++ Scripts/systemprep/setup.sh
@@ -8,17 +8,38 @@ CompType=0
 ###############################################################################
 #Function Section
 
+#Function for Installing Packages
+InstallPackages() {
+	case $1 in
+		"Null" )
+			echo "No supported package managers were detected exiting"
+		;;
+		"apt" )
+			
+		;;
+		"pacman" )
+			
+		;;
+	esac
+	
+}
+
+#Function for creating symlinks for all folders in a directory at the specified location
+SymlinkDirectory(){
+	
+}
+
 ###############################################################################
 #Determine Variables
 #Package Manager Detection
-if ! command -v apt &> /dev/null
-then
+if ! command -v apt &> /dev/null; then
 	echo "Package Manager apt detected"
+	PManager="apt"
 fi
 
-if ! command -v pacman &> /dev/null
-then
+if ! command -v pacman &> /dev/null; then
 	echo "Package Manager pacman detected"
+	PManager="pacman"
 fi
 
 #Prompt for System Type
